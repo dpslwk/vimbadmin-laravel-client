5bab0993293c146049a2a82d3c65993fc6b50015
dpslwk <dps.lwk@gmail.com>
2017-01-02 23:19:28 +0000

    First pass at config

---
+++ config/vimbadmin.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | API URL
+    |--------------------------------------------------------------------------
+    |
+    | Url to your vimbadmin-api endpoint
+    */
+    'api_url' => env('VIMBADMIN_URL', 'http://vimbadmin-api.dev'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Client ID and secret
+    |--------------------------------------------------------------------------
+    |
+    | Your client id and secert are used to get osauth tokens
+    */
+    'client_id' => env('VIMBADMIN_ID'),
+    'client_secret' => env('VIMBADMIN_SECRET'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Token store provider
+    |--------------------------------------------------------------------------
+    |
+    | This option controles which drive is used to store and retrive oAuth
+    | tokens.
+    |
+    | Supported: "json", "eloquent", "doctorine"
+    |
+    */
+    'provider' => env('VIMBADMIN_PROVIDER', 'json'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This defines how the how the Tokens are actually retrieved out of your
+    | database or other storage mechanisms used by this application to
+    | persist your user's data.
+    */
+    'providers' => [
+        'json' => [
+            'file' => env('VIMBADMIN_FILE', 'tokens.json'),
+        ],
+        'eloquent' => [
+            'model' => env('VIMBADMIN_MODEL'),
+        ],
+        'docrtine' => [
+            'model' => env('VIMBADMIN_MODEL'),
+        ],
+
+
+    ],
+
+];
