a1a5adf8feb24e69021f7c878a12741631354111
dpslwk <dps.lwk@gmail.com>
2017-01-04 01:02:56 +0000

    Extend OAuthMiddleware

---
+++ src/Middelware/ViMbAdminOAuthMiddleware.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace LWK\ViMbAdmin\Middelware;
+
+use GuzzleHttp\ClientInterface;
+use LWK\ViMbAdmin\Contracts\TokenStore;
+use Sainsburys\Guzzle\Oauth2\Middleware\OAuthMiddleware;
+use Sainsburys\Guzzle\Oauth2\GrantType\GrantTypeInterface;
+use Sainsburys\Guzzle\Oauth2\GrantType\RefreshTokenGrantTypeInterface;
+
+class ViMbAdminOAuthMiddleware extends OAuthMiddleware
+{
+    /**
+     * @var TokenStore
+     */
+    protected $tokenStore;
+
+    /**
+     * Create a new Oauth2 subscriber.
+     *
+     * @param ClientInterface                $client
+     * @param GrantTypeInterface             $grantType
+     * @param RefreshTokenGrantTypeInterface $refreshTokenGrantType
+     * @param TokenStore                     $tokenStore
+     */
+    public function __construct(
+        ClientInterface $client,
+        GrantTypeInterface $grantType = null,
+        RefreshTokenGrantTypeInterface $refreshTokenGrantType = null,
+        TokenStore $tokenStore
+    ) {
+        $this->tokenStore = $tokenStore;
+        parent::__construct($client, $grantType, $refreshTokenGrantType);
+    }
+
+    /**
+     * Get a new access token.
+     *
+     * @return AccessToken|null
+     */
+    protected function acquireAccessToken()
+    {
+        parent::acquireAccessToken();
+
+        $this->saveAccessToken();
+
+        return $this->accessToken;
+    }
+
+    /**
+     * Save new access token to out tokenStore.
+     */
+    private function saveAccessToken() {
+        $key = $this->tokenStore->serializeKey($this->grantType);
+        $token = $this->tokenStore->create(
+            $key,
+            $this->accessToken->getToken(),
+            $this->accessToken->getExpires(),
+            $this->accessToken->getType()
+        );
+        $this->tokenStore->save($token);
+    }
+}
